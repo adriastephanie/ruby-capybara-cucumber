f799065c7765166951a33f157719ffabb8605b9c
“Adria” <adriastephanie@hotmail.com>
2021-09-01 21:14:26 -0300

    fest: criando ambiente headless

--- tests/features/support/env.rb
+++ tests/features/support/env.rb
@@ -10,19 +10,27 @@ CONFIG = YAML.load_file(File.dirname(__FILE__) + "/ambientes/#{AMBIENTE}.yml")
 
 Capybara.register_driver :selenium do |app|
 
-if BROWSER.eql?('chrome')
-    Capybara::Selenium::Driver.new(app, :browser => :chrome)
+    if BROWSER.eql?('chrome') 
+        Capybara::Selenium::Driver.new(app, :browser => :chrome,) 
+    elsif BROWSER.eql?('chrome_headless')
+        args = ['--start-maximized', '--headless']
+        caps = Selenium::WebDriver::Remote::Capabilities.chrome('goog:chromeOptions' => { 'args' => args })
+        Capybara::Selenium::Driver.new(app, browser: :chrome, desired_capabilities: caps)
+        
+    elsif BROWSER.eql?('firefox')
+        Capybara::Selenium::Driver.new(app, :browser => :firefox, :marionette =>true) 
 
-elsif BROWSER.eql?('firefox')
-    Capybara::Selenium::Driver.new(app, :browser => :firefox, :marionette =>true)
-
-elsif BROWSER.eql?('ie')  
-    Capybara::Selenium::Driver.new(app, :browser => :internet_explorer)
+    elsif BROWSER.eql?('firefox_headless')
+        browser_options = Selenium::WebDriver::Firefox::Options.new(args: ['--headless'])
+        Capybara::Selenium::Driver.new(
+            app, :browser => :firefox, 
+            options: browser_options) 
+    elsif BROWSER.eql?('ie')
+        Capybara::Selenium::Driver.new(app, :browser => :internet_explorer) 
+    elsif BROWSER.eql?('safari')
+        Capybara::Selenium::Driver.new(app, :browser => :safari) 
     
-elsif BROWSER.eql?('safari')  
-    Capybara::Selenium::Driver.new(app, :browser => :safari)
-
-end
+    end
 
 end
 
